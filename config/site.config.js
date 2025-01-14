2fec5913618d2928676aba7d5866a0064ae12b38
Gabby <81215317+ChivalryBlue@users.noreply.github.com>
2025-01-14 13:10:03 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -52,7 +52,7 @@ module.exports = {
   // you provide. See the documentation for details.
   links: [
     {
-      name: 'TG',
+      name: 'Telegram',
       link: 'https://t.me/anti_fraudX_bot',
     },
   ],
